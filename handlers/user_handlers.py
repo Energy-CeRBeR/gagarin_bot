90cf49894a772e8a5f4788a90713493b889a064f
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-04-12 23:59:16 +0300

    Some updates in database structure, create new messages and start to create the next handler

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -6,7 +6,7 @@ from aiogram.types import Message, CallbackQuery
 
 from messages.messages import USER_MESSAGES, USER_COMMANDS
 
-from database.database import users_db, db_template
+from database.database import user_db, db_template
 
 from memoryCode import queries
 
@@ -19,7 +19,16 @@ async def start_bot(message: Message):
     await message.answer(text=USER_COMMANDS[message.text]["in_base"])
 
     # Проверка, привязал ли пользователь тг к сайту
-    if message.from_user.id not in users_db:
+    if message.from_user.id not in user_db:
+        user_db[message.from_user.id] = deepcopy(db_template)
         await message.answer(text=USER_COMMANDS[message.text]["no_base"])
 
     await message.answer(text=USER_COMMANDS[message.text]["in_base"])
+
+
+# Хэндлер для просмотра профиля. Если пользователь авторизовался через сайт
+# (с помощью почты и пароля от соответствующего сайта), то вывод информации о профиле.
+# В противном случае предложить авторизоваться
+@router.message(Command(commands="profile"))
+async def get_profile_info(message: Message):
+    pass
