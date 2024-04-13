ad12ecd7958d4998deef04dac4bfa373eacab1e7
admin5225 <f89155961442@yandex.ru>
2024-04-13 17:16:38 +0300

    date validate

--- handlers/temp_survey_handlers.py
+++ handlers/temp_survey_handlers.py
@@ -1,20 +1,18 @@
 from copy import deepcopy
-from validate_email import validate_email
 
 from aiogram import Router, F
-from aiogram.filters import CommandStart, Command, CommandObject, StateFilter
+from aiogram.filters import CommandStart, Command, StateFilter
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message
 
-from messages.messages import USER_MESSAGES, USER_COMMANDS, SECTION_1_QUESTIONS
+from messages.messages import USER_MESSAGES, USER_COMMANDS
+from messages.messages_questions import SECTION_1_QUESTIONS, ERROR_MESSAGE
+
+from .check_date import check_date # Проверка на корректность введённой даты
 
 from database.database import user_db, db_template
 
-from keyboards.keyboards import create_pages_keyboard
-
-from memoryCode.queries import get_token, get_pages, update_page
-
-from states.states import UserSurveyStates, AuthState
+from states.states import UserSurveyStates
 
 router = Router()
 
@@ -35,20 +33,23 @@ async def start_survey(message: Message, state: FSMContext):
 
     # Задаётся вопрос с нужным индексом
     await message.answer(text=SECTION_1_QUESTIONS[user_db[message.from_user.id]['question_index']])
-    user_db[message.from_user.id]['question_index'] += 1
 
     await state.set_state(UserSurveyStates.survey_section_1)
-
-
-@router.message(Command(commands="profile"))
-async def get_profile_info(message: Message):
-    pass
 
 
 # Обработка вопросов секции 1
 @router.message(UserSurveyStates.survey_section_1)
 async def section_1_processing(message: Message, state: FSMContext):
+    if 'Дата' in SECTION_1_QUESTIONS[user_db[message.from_user.id]['question_index']]:
+        if not check_date(message.text):
+            await message.answer(ERROR_MESSAGE)
+            return True
+
+    await message.answer('ок')
     user_db[message.from_user.id]['section_1_answers'].append(message.text)
+
+    user_db[message.from_user.id]['question_index'] += 1
     if user_db[message.from_user.id]['question_index'] < len(SECTION_1_QUESTIONS):
         await message.answer(text=SECTION_1_QUESTIONS[user_db[message.from_user.id]['question_index']])
+
         user_db[message.from_user.id]['question_index'] += 1
